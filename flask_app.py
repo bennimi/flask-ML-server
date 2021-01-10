915ebed7393e1a0c8b209219abab99b2a2a33a64
bennimi <60404461+bennimi@users.noreply.github.com>
2021-01-10 01:06:56 +0100

    add file upload

--- flask_app.py
+++ flask_app.py
@@ -5,10 +5,11 @@
 info: flask app
 """
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import numpy as np
-import pickle, sys, time, itertools, collections, glob
+import pickle, sys, time, itertools, collections, glob, datetime
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class TextTransformer(BaseEstimator, TransformerMixin):
@@ -26,47 +27,77 @@ path = "/usr/app/"
 model_name = 'svc'
 
 app = Flask(__name__) #run when .py is called 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+db = SQLAlchemy(app)
+
+# class StoreTweets(db.Model):
+#     id = db.Columns(db.Integer,primary_key=True)
+#     timestamp = db.Columns(db.DateTime,default=datetime.datetime.now)
+#     tweet = db.Columns(db.String(),nullable=True)
+    
+#     def __repr__(self):
+#         return '<tweet %r>' %self.id 
+
 
 #all_files = glob.glob(path+"*.pkl")
-
 model = pickle.load(open(path+model_name+".pkl", 'rb'))
+
+context = {'title':"predict tweets",
+               'header':"About this page"}
+
+@app.route('/')
+def project_home():
+    return render_template('about.html', context=context)
 
 @app.route('/about')
 def project_about():
-    context = {'title':"About",
-               'header':"About this page"}
-
     return render_template('about.html', context=context)
 
 
 @app.route('/tweet',methods=['GET','POST'])
 def model_predict():
-    context = {'title':"predict tweets",
-               'header':"About this page"}
-
+    if request.method == 'GET':
+        return render_template('single_predict.html', context=context)
+    
     in_text = request.form.get('input_tweet')   
-    #in_text = "This is a test to see if I am happy"
+    try:
+        pred = model.predict(pd.DataFrame([in_text], columns = ['tweet']))
+    except:
+        redirect('/tweet')
+    try:
+        db.session.add()
+        db.session.commit()
+    except:
+        pass
     
-    if not in_text:
-        return render_template('single_predict.html',context=context)
-    
-    pred = model.predict(pd.DataFrame([in_text], columns = ['tweet']))
-
-    #if pred[0] == 4: return "This is a postive tweet"
-    #if pred[0] == 0: return "This is a negative tweet"
+    if pred [0] == 4: pred = "This is a positive Tweet"
+    else: pred = "This is a negative Tweet"
     return render_template('single_predict.html',
-                           context=context,pred=pred[0],
+                           context=context,
+                           pred=pred,
                            in_text=in_text)
 
-@app.route('/tweets_csv',methods=['POST'])
+@app.route('/file',methods=['GET','POST'])
 def model_predict_file():
-    in_file = pd.read_csv(request.files.get('csv_file'),names=['tweet'])
-    preds = model.predict(in_file)
     
-    preds_percentage = collections.Counter(preds)
-    preds_percentage_neg = round(preds_percentage[0]/preds,2)
+    if request.method == 'POST':
+        in_file = request.files.get('input_file')
+        print(in_file)
+        # in_file = pd.read_csv(,names=['tweet'])
+        # preds = model.predict(in_file)
+        # preds_percentage = collections.Counter(preds)
+        # preds_percentage_neg = round(preds_percentage[0]/preds,2)
+        # print(preds_percentage_neg)
+        return render_template('file_predict.html',context=context)
+        
+    else:
+       return render_template('file_predict.html',context=context)
     
-    return str(preds_percentage_neg) + " of tweets are negative\n\n" + str((preds))
+
+    #return str(preds_percentage_neg) + " of tweets are negative\n\n" + str((preds))
+
+
 if __name__ == '__main__':
     #app.run()
     app.run(host='0.0.0.0',port=8000) #,debug=True)
